7610d798fc6ee6c6dce18b2c56393f652b5eb2d4
Felicio Almeida <feliciorar@gmail.com>
2024-02-26 15:31:51 -0300

    added responsitivity

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
-import NotFound from "./pages/Not Found";
 
 function AppRoutes () {
     return (
@@ -11,7 +10,6 @@ function AppRoutes () {
                 <Route path="/" element={ <Home /> }></Route>
                 <Route path="/login" element={ <Login /> }></Route>
                 <Route path="/register" element={ <Register /> }></Route>
-                <Route path="*" element={ <NotFound /> }></Route>
             </Routes>
         </BrowserRouter>
     )
