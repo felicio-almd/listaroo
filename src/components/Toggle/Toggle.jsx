e68b907c474de239deb15f025110322acad9c017
Felicio Almeida <feliciorar@gmail.com>
2024-04-29 09:57:31 -0300

    :sparkles: add sun and moon on toogle page mode

--- src/components/Toggle/Toggle.jsx
+++ src/components/Toggle/Toggle.jsx
@@ -2,21 +2,21 @@ import { useState } from 'react'
 import { Icon } from '@iconify/react';
 import "./Toggle.css"
 
-export const Toggle = ({ toggled, onClick, toggleTheme }) => {
+export const Toggle = ({ toggled, toggleTheme }) => {
     const [isToggled, toggle] = useState(toggled);
 
     const callback = () => {
         toggle(!isToggled);
-        toggleTheme(); // Chama a função para alternar o tema
+        toggleTheme();
     };
 
     return (
-        <label className='label__toggle'>
-            <input className='input__dark' type="checkbox" defaultChecked={isToggled} onClick={callback} />
-            <span className='span__toggle'>
-                {/* <Icon icon="material-symbols:mode-night" width="30" height="30"/> */}
-                {/* <Icon icon="material-symbols:clear-day-rounded" width="30" height="30"/> */}
-            </span>
-        </label>
+        <label className={`label__toggle ${isToggled ? 'dark-mode' : ''}`}>
+          <input className='input__dark' type="checkbox" defaultChecked={isToggled} onClick={callback} />
+          <span className='span__toggle'>
+            <Icon className={`span__icon__sun ${isToggled ? 'hidden' : ''}`} icon="material-symbols:clear-day-rounded" width="22" height="22"/>
+            <Icon className={`span__icon__moon ${isToggled ? '' : 'hidden'}`} icon="material-symbols:mode-night" width="22" height="22"/>
+          </span>
+        </label>      
     );
 };
