aede002925b0fd3d8f3538684fefd040968cd9dc
Felicio Almeida <feliciorar@gmail.com>
2024-04-29 10:21:03 -0300

    :wastebasket: remove register route

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home"
 import Login from "./pages/Login"
-import Register from "./pages/Register"
 
 function AppRoutes () {
     return (
@@ -9,7 +8,6 @@ function AppRoutes () {
             <Routes>
                 <Route path="/" element={ <Home /> }></Route>
                 <Route path="/login" element={ <Login /> }></Route>
-                <Route path="/register" element={ <Register /> }></Route>
             </Routes>
         </BrowserRouter>
     )
