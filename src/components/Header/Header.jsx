86d9cf3c30fed3238b28b3b8e08726a3d293ebb5
Felicio Almeida <feliciorar@gmail.com>
2024-04-29 12:02:59 -0300

    :lipstick: feat: add show username to home

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,10 +4,12 @@ import "./Header.css"
 
 
 function Header ({children}) {
+    const storedUsername = localStorage.getItem("username");
     
     return (
         <header className="header">
-            <Link className="header__title">Listaroo</Link>           
+            <Link to='/' className="header__title">Listaroo</Link>
+            <div className="header__user">{storedUsername}</div>           
             {children}
         </header>
     )
