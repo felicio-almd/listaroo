18b2221e5c39387896ac74bf63f53141454bd767
Felicio Almeida <feliciorar@gmail.com>
2024-04-29 12:02:07 -0300

    :ok_hand: update routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,16 +1,23 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home"
-import Login from "./pages/Login"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 
-function AppRoutes () {
+function AppRoutes() {
+    const storedUsername = localStorage.getItem("username");
+    
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={ <Home /> }></Route>
-                <Route path="/login" element={ <Login /> }></Route>
+                {storedUsername ? (
+                    <Route path="/" element={<Home />} />
+                ) : (
+                    <Route path="/" element={<Navigate to="/login" />} />
+                )}
+                <Route path="/login" element={<Login />} />
+                <Route path="/home" element={<Home />} />
             </Routes>
         </BrowserRouter>
-    )
+    );
 }
 
-export default AppRoutes
+export default AppRoutes;
